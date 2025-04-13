f534427e7bd0a46fa898e521c79097efd295a31a
Abdellah El Morabit <nsrddyn@gmail.com>
2025-04-13 17:45:04 +0200

    first version of the handler

--- main.go
+++ main.go
@@ -1,47 +1,58 @@
-package main 
+package main
 
 import (
-	"fmt"
-	"github.com/gorilla/websocket"
 	"net/http"
 	"time"
 	"log"
+	"github.com/gorilla/websocket"
 )
 
 var upgrader = websocket.Upgrader{
-    ReadBufferSize:  1024,
-    WriteBufferSize: 1024,
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
 	EnableCompression: true,
-
+	CheckOrigin: func(r *http.Request) bool {
+		//		return r.Host == "nsrddyn.com"	
+		return true
+	},
 }
 
 func wsHandler(w http.ResponseWriter, r *http.Request){
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		fmt.Println("Error upgrading", err)
+		log.Printf("Error upgrading to WebSocket :%v\n", err)
 		return
 	}
 	defer conn.Close()
-	for {
-		sendData := Sys.CPU()
 
+	for {
+		dataType, data, err := conn.ReadMessage()
+		if err != nil {
+			log.Printf("Error reading WebSocket message: %v\n", err)
+			break
+		}
+
+		log.Printf("Received: %s\n", data)
+
+		if err := conn.WriteMessage(dataType, data); err != nil {
+			log.Printf("Error writing WebSocket message: %v\n", err)
+			break
+		}
 	}
 }
 
 func main(){
+
 	server := &http.Server{
 		Addr: ":8085",
-		Handler: wsHandler,
+		Handler: http.HandlerFunc(wsHandler), 
 		ReadTimeout: 10 * time.Second,
 		WriteTimeout: 10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
-
-	http.HandleFunc("/ws", wsHandler)
-	fmt.Println("Websocket server started on :8080")
-	log.Fatal(server.ListenAndServe)
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Println("Error starting server: ", err)
+	err := server.ListenAndServe()
+	if err != nil{
+		log.Fatalf("Failed to listen and serve the server %v\n", err)
 	}
 }
+
